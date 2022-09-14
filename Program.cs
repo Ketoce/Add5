40e67c6a7ed10a85432cfc30f4549a1d250c04a6
Ketoce <Timakova-2017@inbox.ru>
2022-09-14 12:33:49 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string lang = "ru";
+
+            switch (lang)
+            {
+                case "ru":
+                    Console.WriteLine("Алина сломала проект!");
+                    break;
+                case "en":
+                    Console.WriteLine("Alina broke the project!");
+                    break;
+                case "ua":
+                    Console.WriteLine("Аліна зламала проект!");
+                    break;
+                case "az":
+                    Console.WriteLine("Alina layihəni pozdu!");
+                    break;
+                case "de":
+                    Console.WriteLine("Alina hat das Projekt abgebrochen!");
+                    break;
+                case "sw":
+                    Console.WriteLine("Alina bröt projektet!");
+                    break;
+                case "ar":
+                    Console.WriteLine("كسرت ألينا المشروع!");
+                    break;
+                case "tt":
+                    Console.WriteLine("Алина проектны бозды!");
+                    break;
+                case "gr":
+                    Console.WriteLine("ალინამ პროექტი დაარღვია");
+                    break;
+
+
+
+
+
+            }
+        }
+    }
+}
+
